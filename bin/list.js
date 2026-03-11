5201673c34f4653bdf4e027b589954fc7c422f63
emscripten nightly action <builds@argot.org>
2026-03-11 02:12:58 +0000

    Nightly build v0.8.35-nightly.2026.3.11+commit.9f4d0b87

--- bin/list.js
+++ bin/list.js
@@ -1,5 +1,6 @@
 
 var soljsonSources = [
+  "soljson-v0.8.35-nightly.2026.3.11+commit.9f4d0b87.js",
   "soljson-v0.8.35-nightly.2026.3.9+commit.63131344.js",
   "soljson-v0.8.35-nightly.2026.3.5+commit.61baed53.js",
   "soljson-v0.8.35-nightly.2026.3.3+commit.0409c5c6.js",
--- bin/soljson-nightly.js
+++ bin/soljson-nightly.js
@@ -1,1 +1,1 @@
-soljson-v0.8.35-nightly.2026.3.9+commit.63131344.js
+soljson-v0.8.35-nightly.2026.3.11+commit.9f4d0b87.js
